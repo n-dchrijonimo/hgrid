98fba6e527746178f9ea3f1b44ca74ec4c23f927
Christo <43973126+n-dchrijonimo@users.noreply.github.com>
2022-01-29 00:03:17 -0500

    Update hgrid.cpp

--- hgrid.cpp
+++ hgrid.cpp
@@ -12,6 +12,7 @@ int main(){
   srand((unsigned int)time(NULL)); //sets rng seed
   int cyc=0;
   setup();
+  unsigned short *grid=new unsigned short[x*y]{0); //allocate a 1 d array of length x times y
   populate(int x, int y);
   while(cyc<x*y){
     if(grid[cyc]==0){
@@ -28,18 +29,32 @@ int main(){
 }
 
 void setup{
-  int cyc=0;
+ int cyc=0;
  
   /*we can make another variable called "cyc" here because in c++, variables declared in a function can only be accessed in said function,
   unless they are declared in a header or before the beginning of the main function (these are called global variables). This is known as a variable's scope*/
  
-  string ans; //will store answer from input stream
+ int ans_num=0;
+ string ans; //will store answer from input stream
+ 
+ cout<<"Enter how long you want the grid to be. Positive numbers only please!\n";
+ while(cyc<3){
   getline(cin, ans);
  
-  /*will take the whole line. Normally spaces after characters end the cin function, and any characters after the space would remain
-  in the buffer. Getline is a lot cleaner. More on this later*/
+   /*will take the whole line. Normally spaces after characters end the cin function, and any characters after the space would remain
+   in the buffer. Getline is a lot cleaner. More on this later*/
   
-  inline 
-       
-  unsigned short *grid=new unsigned short[x*y]{0); //allocate a 1 d array of length x times y
+   ans_num=positive_int_only(ans);
+  if(ans_num==-1){
+   cout<<"Input was not a number. Please try again\n";
+  } else if(cyc==0){
+   x=ans_num;
+   cout<<"Nice!\n\nNow enter how tall you want the grid to be!\n";
+   ++cyc;
+ } else if(cyc==1){
+   y=ans_num;
+  cout<<"Radical! That's a mighty fine grid! Now let's let 'er rip!\n";
+ }
+     
+  
 }
