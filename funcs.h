c7a80de9d529eb189691657983f0bb01262930d4
Christo <43973126+n-dchrijonimo@users.noreply.github.com>
2022-01-28 23:43:20 -0500

    Update funcs.h

--- funcs.h
+++ funcs.h
@@ -1,7 +1,13 @@
+#include <string>
+
+using namespace std;
+
 #ifndef FUNCS_H_INCLUDED
 #define FUNCS_H_INCLUDED
+
 //functions
 void setup(); //enter initial info
+int positive_int_only(string);
 
 //vars
 extern int x=0; extern int y=0;
