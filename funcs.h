062d3b8f671c585d49d6f2c7cf9df411900cb5ee
Christo <43973126+n-dchrijonimo@users.noreply.github.com>
2022-01-28 22:17:32 -0500

    Update funcs.h

--- funcs.h
+++ funcs.h
@@ -1,6 +1,10 @@
 #ifndef FUNCS_H_INCLUDED
 #define FUNCS_H_INCLUDED
+//functions
 void setup(); //enter initial info
 
+//vars
+extern int x=0; extern int y=0;
+extern short *grid;
 // FUNCS_H_INCLUDED
 #endif
